aaffe26504484f788f6cf7e11c753d6bae1c0edb
Ken <kabutukenneth3@gmail.com>
2023-06-25 12:21:04 +0300

    completed assignment

---
+++ calc.rb
@@ -0,0 +1,22 @@
+# getting input from user
+
+puts "Enter the first number: "
+num1 = gets.chomp().to_f
+puts "Enter the operator: "
+operator = gets.chomp()
+puts "Enter the second number: "
+num2 = gets.chomp().to_f
+
+#solution using if statement
+
+if operator == "+"
+   print (num1 + num2)
+elsif operator == "-"
+   print (num1 - num2)
+elsif operator == "*"
+   print (num1 * num2)
+elsif operator == "/"
+   print (num1 / num2)
+else
+   puts "Invalid operator"
+end 
